70a25f6844dfa58ef8f7a7917b17f98211bd6c27
luxidea <contact@luxidea.net>
2021-02-25 18:11:25 +0900

    15-draw_cube に必要な関数を追加。

--- vulkan.util_init.pas
+++ vulkan.util_init.pas
@@ -124,6 +124,15 @@ procedure destroy_descriptor_pool( var info_:T_sample_info );
 procedure destroy_vertex_buffer( var info_:T_sample_info );
 procedure destroy_shaders( var info_:T_sample_info );
 
+//////////////////////////////////////////////////////////////////////////////// 15-draw_cube
+
+procedure init_viewports( var info_:T_sample_info );
+procedure init_scissors( var info_:T_sample_info );
+procedure init_pipeline_cache( var info:T_sample_info );
+procedure init_pipeline( var info:T_sample_info; include_depth:VkBool32; include_vi:VkBool32 = 1 );
+procedure destroy_pipeline( var info_:T_sample_info );
+procedure destroy_pipeline_cache( var info_:T_sample_info );
+
 implementation //############################################################### ■
 
 uses System.Types, System.Math, System.SysUtils,
@@ -1413,6 +1422,197 @@ procedure destroy_shaders( var info_:T_sample_info );
 begin
      vkDestroyShaderModule( info_.device, info_.shaderStages[0].module, nil );
      vkDestroyShaderModule( info_.device, info_.shaderStages[1].module, nil );
+end;
+
+//////////////////////////////////////////////////////////////////////////////// 15-draw_cube
+
+procedure init_viewports( var info_:T_sample_info );
+begin
+     info_.viewport.height   := info_.height;
+     info_.viewport.width    := info_.width;
+     info_.viewport.minDepth := 0.0;
+     info_.viewport.maxDepth := 1.0;
+     info_.viewport.x        := 0;
+     info_.viewport.y        := 0;
+     vkCmdSetViewport( info_.cmd, 0, NUM_VIEWPORTS, @info_.viewport );
+end;
+
+procedure init_scissors( var info_:T_sample_info );
+begin
+     info_.scissor.extent.width  := info_.width;
+     info_.scissor.extent.height := info_.height;
+     info_.scissor.offset.x      := 0;
+     info_.scissor.offset.y      := 0;
+     vkCmdSetScissor( info_.cmd, 0, NUM_SCISSORS, @info_.scissor );
+end;
+
+procedure init_pipeline_cache( var info:T_sample_info );
+var
+   res :VkResult;
+   pipelineCache :VkPipelineCacheCreateInfo;
+begin
+     pipelineCache.sType           := VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+     pipelineCache.pNext           := nil;
+     pipelineCache.initialDataSize := 0;
+     pipelineCache.pInitialData    := nil;
+     pipelineCache.flags           := 0;
+     res := vkCreatePipelineCache( info.device, @pipelineCache, nil, @info.pipelineCache );
+     Assert( res = VK_SUCCESS );
+end;
+
+procedure init_pipeline( var info:T_sample_info; include_depth:VkBool32; include_vi:VkBool32 = 1 );
+var
+   res :VkResult;
+   dynamicStateEnables :array [ 0..2-1 ] of VkDynamicState;  // Viewport + Scissor
+   dynamicState        :VkPipelineDynamicStateCreateInfo;
+   vi                  :VkPipelineVertexInputStateCreateInfo;
+   ia                  :VkPipelineInputAssemblyStateCreateInfo;
+   rs                  :VkPipelineRasterizationStateCreateInfo;
+   cb                  :VkPipelineColorBlendStateCreateInfo;
+   att_state           :array [ 0..1-1 ] of VkPipelineColorBlendAttachmentState;
+   vp                  :VkPipelineViewportStateCreateInfo;
+   viewports           :VkViewport;
+   scissor             :VkRect2D;
+   ds                  :VkPipelineDepthStencilStateCreateInfo;
+   ms                  :VkPipelineMultisampleStateCreateInfo;
+   pipeline            :VkGraphicsPipelineCreateInfo;
+begin
+     dynamicState.sType             := VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+     dynamicState.pNext             := nil;
+     dynamicState.pDynamicStates    := @dynamicStateEnables[0];
+     dynamicState.dynamicStateCount := 0;
+
+     vi.sType := VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+     if include_vi <> 0 then
+     begin
+          vi.pNext                           := nil;
+          vi.flags                           := 0;
+          vi.vertexBindingDescriptionCount   := 1;
+          vi.pVertexBindingDescriptions      := @info.vi_binding;
+          vi.vertexAttributeDescriptionCount := 2;
+          vi.pVertexAttributeDescriptions    := @info.vi_attribs[0];
+     end;
+     ia.sType                  := VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+     ia.pNext                  := nil;
+     ia.flags                  := 0;
+     ia.primitiveRestartEnable := VK_FALSE;
+     ia.topology               := VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+
+     rs.sType                   := VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+     rs.pNext                   := nil;
+     rs.flags                   := 0;
+     rs.polygonMode             := VK_POLYGON_MODE_FILL;
+     rs.cullMode                := Ord( VK_CULL_MODE_BACK_BIT );
+     rs.frontFace               := VK_FRONT_FACE_CLOCKWISE;
+     rs.depthClampEnable        := VK_FALSE;
+     rs.rasterizerDiscardEnable := VK_FALSE;
+     rs.depthBiasEnable         := VK_FALSE;
+     rs.depthBiasConstantFactor    := 0;
+     rs.depthBiasClamp             := 0;
+     rs.depthBiasSlopeFactor       := 0;
+     rs.lineWidth                  := 1.0;
+
+     cb.sType := VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+     cb.flags := 0;
+     cb.pNext := nil;
+     att_state[0].colorWriteMask      := $f;
+     att_state[0].blendEnable         := VK_FALSE;
+     att_state[0].alphaBlendOp        := VK_BLEND_OP_ADD;
+     att_state[0].colorBlendOp        := VK_BLEND_OP_ADD;
+     att_state[0].srcColorBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].dstColorBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].srcAlphaBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].dstAlphaBlendFactor := VK_BLEND_FACTOR_ZERO;
+     cb.attachmentCount   := 1;
+     cb.pAttachments      := @att_state[0];
+     cb.logicOpEnable     := VK_FALSE;
+     cb.logicOp           := VK_LOGIC_OP_NO_OP;
+     cb.blendConstants[0] := 1.0;
+     cb.blendConstants[1] := 1.0;
+     cb.blendConstants[2] := 1.0;
+     cb.blendConstants[3] := 1.0;
+
+     vp.sType := VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+     vp.pNext := nil;
+     vp.flags := 0;
+     // Temporary disabling dynamic viewport on Android because some of drivers doesn't
+     // support the feature.
+     viewports.minDepth := 0.0;
+     viewports.maxDepth := 1.0;
+     viewports.x        := 0;
+     viewports.y        := 0;
+     viewports.width    := info.width;
+     viewports.height   := info.height;
+     scissor.extent.width  := info.width;
+     scissor.extent.height := info.height;
+     scissor.offset.x      := 0;
+     scissor.offset.y      := 0;
+     vp.viewportCount := NUM_VIEWPORTS;
+     vp.scissorCount  := NUM_SCISSORS;
+     vp.pScissors     := @scissor;
+     vp.pViewports    := @viewports;
+     ds.sType                 := VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+     ds.pNext                 := nil;
+     ds.flags                 := 0;
+     ds.depthTestEnable       := include_depth;
+     ds.depthWriteEnable      := include_depth;
+     ds.depthCompareOp        := VK_COMPARE_OP_LESS_OR_EQUAL;
+     ds.depthBoundsTestEnable := VK_FALSE;
+     ds.stencilTestEnable     := VK_FALSE;
+     ds.back.failOp           := VK_STENCIL_OP_KEEP;
+     ds.back.passOp           := VK_STENCIL_OP_KEEP;
+     ds.back.compareOp        := VK_COMPARE_OP_ALWAYS;
+     ds.back.compareMask      := 0;
+     ds.back.reference        := 0;
+     ds.back.depthFailOp      := VK_STENCIL_OP_KEEP;
+     ds.back.writeMask        := 0;
+     ds.minDepthBounds        := 0;
+     ds.maxDepthBounds        := 0;
+     ds.stencilTestEnable     := VK_FALSE;
+     ds.front                 := ds.back;
+
+     ms.sType                  := VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+     ms.pNext                  := nil;
+     ms.flags                  := 0;
+     ms.pSampleMask            := nil;
+     ms.rasterizationSamples   := NUM_SAMPLES;
+     ms.sampleShadingEnable    := VK_FALSE;
+     ms.alphaToCoverageEnable := VK_FALSE;
+     ms.alphaToOneEnable      := VK_FALSE;
+     ms.minSampleShading      := 0.0;
+
+     pipeline.sType               := VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+     pipeline.pNext               := nil;
+     pipeline.layout              := info.pipeline_layout;
+     pipeline.basePipelineHandle  := VK_NULL_HANDLE;
+     pipeline.basePipelineIndex   := 0;
+     pipeline.flags               := 0;
+     pipeline.pVertexInputState   := @vi;
+     pipeline.pInputAssemblyState := @ia;
+     pipeline.pRasterizationState := @rs;
+     pipeline.pColorBlendState    := @cb;
+     pipeline.pTessellationState  := nil;
+     pipeline.pMultisampleState   := @ms;
+     pipeline.pDynamicState       := @dynamicState;
+     pipeline.pViewportState      := @vp;
+     pipeline.pDepthStencilState  := @ds;
+     pipeline.pStages             := @info.shaderStages[0];
+     pipeline.stageCount          := 2;
+     pipeline.renderPass          := info.render_pass;
+     pipeline.subpass             := 0;
+
+     res := vkCreateGraphicsPipelines(info.device, info.pipelineCache, 1, @pipeline, nil, @info.pipeline);
+     assert( res = VK_SUCCESS );
+end;
+
+procedure destroy_pipeline( var info_:T_sample_info );
+begin
+     vkDestroyPipeline( info_.device, info_.pipeline, nil );
+end;
+
+procedure destroy_pipeline_cache( var info_:T_sample_info );
+begin
+     vkDestroyPipelineCache( info_.device, info_.pipelineCache, nil );
 end;
 
 end. //######################################################################### ■
